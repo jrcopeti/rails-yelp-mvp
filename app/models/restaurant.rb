bf6fc246bba6d9122fbf8a9be59638ac599ff967
Jose Copeti <jrcopeti@gmail.com>
2023-01-27 07:24:31 +0100

    challenge completed not too much front end style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
   validates :name, :address, :category, :phone_number, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :category, inclusion: { in: CATEGORIES }
+  has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@
 puts 'empty database'
 
 puts 'Creating restaurants...'
+
+20.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "restaurants with is #{restaurant} has been created"
+end
+
+puts 'finished'
